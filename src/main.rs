edf5ff7d08c984aaa4c4b9c1a544367cc9341a22
QuoteQuack <118599418+quotequack@users.noreply.github.com>
2024-10-08 19:40:01 +0300

    fixed it up

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,43 @@ use enigo::*;
 use fltk::{app, button::Button, frame::Frame, input::{self, Input}, prelude::*, window::Window};
 
 fn main() {
-    let mut toplace = vec![];
     let app = app::App::default();
     let mut wind = Window::new(100, 100, 400, 300, "RustyTyper");
 
-    let mut inp = Input::new(0, 0, 130, 150, "input");
-    let mut delay = Input::new(131, 0, 130, 150, "delay");
-    let mut add = Button::new(261, 0, 65, 150, "+");
-    let mut times = Input::new(326, 0, 65, 150, "X?");
+    let mut inp = Input::new(50, 0, 130, 30, "Input");
+    let mut delay = Input::new(50, 40, 130, 30, "Delay");
+    let mut add = Button::new(270, 0, 65, 30, "+");
+    let mut times = Input::new(360, 0, 65, 30, "X?");
+    
+    let mut outp = Frame::new(50, 40, 400, 30, "Output:");
+    let mut gen = Button::new(0, 80, 400, 30, "Generate");
 
-    let mut outp = Frame::new(0, 151, 260, 150, "output");
-    let mut gen = Button::new(261, 151, 130, 150, "gen");
+    let toplace = std::rc::Rc::new(std::cell::RefCell::new(Vec::new()));
+    let toplace_add = toplace.clone();
+    let toplace_gen = toplace.clone();
 
     add.set_callback(move |_| {
-        if inp.value().len() > 0 {
-            for i in times.value().parse<i32>().unwrap() {
-                toplace.push(inp.value());
+        if let Ok(repeat) = times.value().parse::<usize>() {
+            if !inp.value().is_empty() {
+                let mut toplace = toplace_add.borrow_mut();
+                for _ in 0..repeat {
+                    toplace.push(inp.value().clone());
+                }
+                outp.set_label(&toplace.join(", "));
             }
         }
     });
+
     gen.set_callback(move |_| {
-        outp.set_label(toplace.join("").as_str());
-        let mut stn = enigo::Enigo::new(&Settings::default()).unwrap();
-        for i in &toplace {
-            stn.text(i);
-            std::thread::sleep(std::time::Duration::from_secs(delay.value().parse::<u64>().unwrap()));
+        let delay_ms = delay.value().parse::<u64>().unwrap_or(100); // Default to 100 ms
+        let mut enigo = Enigo::new(&Settings::default()).unwrap();
+        for text in toplace_gen.borrow().iter() {
+            enigo.text(text);
+            std::thread::sleep(std::time::Duration::from_millis(delay_ms));
         }
     });
 
     wind.end();
     wind.show();
-    app.run();
+    app.run().unwrap();
 }
